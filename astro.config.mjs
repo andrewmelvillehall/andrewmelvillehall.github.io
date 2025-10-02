61f114fe08067a4743cf096ac10ba8240b2954a6
Zoe Nickson <mnickson@sidingsmedia.com>
2025-10-02 21:27:56 +0100

    Deploy
    Signed-off-by: Zoe Nickson <mnickson@sidingsmedia.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // Replace with your site URL
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
+  site: "https://andrewmelvillehall.github.io", // Replace with your site URL
+  base: "website",
+  integrations: [tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'github-light',
-      wrap: true
-    }
-  }
+      theme: "github-light",
+      wrap: true,
+    },
+  },
 });
